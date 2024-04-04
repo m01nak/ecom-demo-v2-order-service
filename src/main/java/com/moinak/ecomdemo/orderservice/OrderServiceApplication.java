9ff87d5a0543d31a6e3f96e5beafb1f0edf22dae
m01nak <m01nak77@gmail.com>
2024-04-04 10:02:29 +0530

    init

---
+++ src/main/java/com/moinak/ecomdemo/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.moinak.ecomdemo.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moinak/ecomdemo/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moinak.ecomdemo.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
